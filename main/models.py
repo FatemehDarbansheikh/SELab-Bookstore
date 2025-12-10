ba2543616ecfc8708e835580145774849b41a009
FatemehDarbansheikh <fa.drsh1401@gmail.com>
2025-12-10 20:09:27 +0330

    Finishing models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,149 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=20)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.country}"
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    ISBN = models.CharField(max_length=20, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+    description = models.TextField(blank=True, null=True)
+    publication_date = models.DateField(blank=True, null=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name= 'books')
+    authors = models.ManyToManyField(Author, related_name='books')
+    categories = models.ManyToManyField(Category, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True, null=True)
+    review_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name='reviews')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name= 'reviews')
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    book = models. ForeignKey(Book, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.IntegerField()
+
+    class Meta:
+        unique_together=('user','book')
+
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlists')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='wishlists')
+    added_date = models. DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together=('user','book')
+
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=30)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'book')
+
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='payments')
+    payment_method = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_status = models.CharField(max_length=30)
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+
+
+class Support(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='supports')
+    admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True, blank=True, related_name='supports')
+    subject = models.CharField(max_length=150)
+    message = models.CharField(max_length=255)
+    status = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True, blank=True, related_name='notifications')
+    message = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+
