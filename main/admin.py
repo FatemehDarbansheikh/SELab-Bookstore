81fdb75a74dfb32795bf2076531edfc2b73583f8
FatemehDarbansheikh <fa.drsh1401@gmail.com>
2025-12-10 21:56:31 +0330

    admin.py and testing

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import (
+    User, Admin, Address, Publisher, Author, Category, Book,
+    Review, Cart, Wishlist, Order, OrderItem, Payment, Support, Notification
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Admin)
+admin.site.register(Address)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Cart)
+admin.site.register(Wishlist)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+admin.site.register(Support)
+admin.site.register(Notification)
