39543364c41fde155ec526cce83574ae6c35e1c8
FatemehDarbansheikh <fa.drsh1401@gmail.com>
2026-02-07 23:34:09 +0330

    context_pro

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,6 +1,7 @@
 from .models import Notification
 
 def unread_notifications(request):
+    
     if request.user.is_authenticated:
         return {
             'unread_notifications_count': Notification.objects.filter(
